efde98e8ff661290510be45f2479f44cb6354238
Sussuly <Sulyvansayah@gmail.com>
2024-11-22 09:14:36 +0100

    bdd

--- db.client.js
+++ db.client.js
@@ -2,15 +2,19 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // User
+  process.env.DB_PASSWORD, // Password
   {
+    host: process.env.DB_HOST, // Host
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  },
+  }
 );
 
 // authentication and synchronization
